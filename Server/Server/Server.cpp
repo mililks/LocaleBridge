aa0373b2808b6c269376316fd062665b5ecac3ed
ERUDIT0217 <dimapv08@gmail.com>
2025-12-05 09:03:43 +0200

    vidkat

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -16,14 +16,12 @@
 using namespace std;
 
 const int TCP_PORT = 5000;
-const int UDP_PORT = 5001;
 
 vector<SOCKET> clientSockets;
 map<SOCKET, string> clientNames;
 mutex clientMutex;
 
 void ClientHandler(SOCKET clientSocket);
-void UdpDiscoveryThread();
 
 int main()
 {
@@ -65,9 +63,6 @@ int main()
         return 0;
     }
     cout << "Сервер прослуховує порт 5000. Очікування клієнтів..." << endl;
-
-    thread udpThread(UdpDiscoveryThread);
-    udpThread.detach();
 
     while (true) {
         sockaddr_in client{};
@@ -179,48 +174,4 @@ void ClientHandler(SOCKET clientSocket) {
     closesocket(clientSocket);
 
     cout << "'" << clientName << "' Клієнт відключився. З'єднання закрито." << endl;
-}
-
-void UdpDiscoveryThread() {
-    SOCKET udpSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (udpSocket == INVALID_SOCKET) {
-        cout << "UDP Помилка створення сокета." << endl;
-        return;
-    }
-
-    sockaddr_in serverAddr{};
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(UDP_PORT);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(udpSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        cout << "UDP Помилка прив'язки." << endl;
-        closesocket(udpSocket);
-        return;
-    }
-    cout << "UDP прослуховує порт " << UDP_PORT << "." << endl;
-
-    char recvBuf[1024];
-    sockaddr_in clientAddr{};
-    int clientAddrSize = sizeof(clientAddr);
-
-    while (true) {
-        int bytesReceived = recvfrom(udpSocket, recvBuf, sizeof(recvBuf), 0, (sockaddr*)&clientAddr, &clientAddrSize);
-
-        if (bytesReceived == SOCKET_ERROR) {
-            if (WSAGetLastError() != 10004) {
-                cout << "UDP Помилка прийому." << endl;
-            }
-            break;
-        }
-
-        string response = "OK:" + to_string(TCP_PORT);
-
-        sendto(udpSocket, response.c_str(), (int)response.length(), 0, (sockaddr*)&clientAddr, clientAddrSize);
-
-        string clientIP = inet_ntoa(clientAddr.sin_addr);
-        cout << "UDP Discovery: відповів клієнту " << clientIP << endl;
-    }
-
-    closesocket(udpSocket);
 }
